be2e05e83c94d1b04c3bbcde703da08f28b481f1
Hiromi Kanou <livestock.infomation@gmail.com>
2026-03-15 17:23:55 +0900

    fix: metro blockList をプロジェクト直下の src のみに限定

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,17 @@
+const path = require("path");
 const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
+
+// src/ は Vite 用の別アプリ。Expo ビルドでは react-router がなくエラーになるため除外
+// node_modules 内の src（例: @expo/metro-runtime）は除外しないよう、プロジェクト直下の src のみ対象にする
+const projectSrcPath = path.resolve(__dirname, "src");
+const projectSrcRegex = new RegExp(
+  "^" + projectSrcPath.replace(/[.*+?^${}()|[\]\\]/g, "\\$&") + "[\\\\/]"
+);
+config.resolver.blockList = [
+  projectSrcRegex,
+  ...(config.resolver.blockList ?? []),
+];
 
 module.exports = config;
